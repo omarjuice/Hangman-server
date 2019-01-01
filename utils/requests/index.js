12451c83f4fd4137544cfca200d9292651e03262
Omar Juice <ojameer1@gmail.com>
2019-01-01 12:43:18 -0500

    edit requests

--- utils/requests/index.js
+++ utils/requests/index.js
@@ -10,16 +10,18 @@ const dict = new Dictionary(app_id, app_key);
 const editUrban = (definition, word) => {
     const regex = new RegExp(`\\[\(\\w+\)\\]`, 'gmi')
     const regex2 = new RegExp(`${word}`, 'gmi')
-    // /\[(\w+)\]/g
-    console.log(regex, regex2)
     return definition.replace(/\[([\w+\s*]+)\]/g, (a, b) => b).replace(regex2, '_'.repeat(word.length))
 }
 const fetchUrban = async (word) => {
     return axios.get(`http://api.urbandictionary.com/v0/define?term={${word}}`)
         .then(({ data }) => {
-            return Promise.resolve(
-                data.list.length > 0 ? editUrban(data.list.sort((a, b) => b.thumbs_up > a.thumbs_up)[0].definition, word) : false
-            )
+            try {
+                return Promise.resolve(
+                    data.list.length > 0 ? editUrban(data.list.sort((a, b) => b.thumbs_up > a.thumbs_up)[0].definition, word) : false
+                )
+            } catch (e) {
+                throw ('No such entry found')
+            }
         }).catch((e) => {
             throw (e)
         })
@@ -30,7 +32,11 @@ const fetchOxford = async word => {
             if (error) {
                 return reject(error)
             }
-            resolve((data.results[0].lexicalEntries[0].entries[0].senses[0].short_definitions[0]))
+            try {
+                resolve((data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]))
+            } catch (e) {
+                reject('No such entry found')
+            }
         })
     })
 }
