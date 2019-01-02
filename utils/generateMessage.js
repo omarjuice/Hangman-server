6722c5c22503a8fbf72481b794f3f6911e3346db
Omar Juice <ojameer1@gmail.com>
2019-01-01 19:10:16 -0500

    date reformatting

--- utils/generateMessage.js
+++ utils/generateMessage.js
@@ -1,10 +1,9 @@
-const moment = require('moment')
 
 const generateMessage = ({ from, text }) => {
     return {
         from,
         text,
-        createdAt: moment().format('h:mm a')
+        createdAt: new Date()
     }
 }
 module.exports = { generateMessage }
